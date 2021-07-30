20d0fdb9a3cb3c2bac9993018b3fca9e94e6c9e4
Alexandre Paes <alexandre_slp@poli.ufrj.br>
2021-07-29 21:52:21 -0300

    refactored with small changes

--- src/config.py
+++ src/config.py
@@ -1,11 +1,11 @@
+import json
 import os
+import platform
+import socket
 import subprocess
 import sys
-import socket
-import platform
-import util
-import json
 
+import util
 
 APP = os.path.basename(sys.argv[0])
 
@@ -22,18 +22,17 @@ if util.on_macos or util.on_windows:
     NAME = platform.uname()[0]
 
 else:
-    NAME = open('/etc/os-release',
-                'r').read().split('NAME="')[1].split('"')[0]
+    NAME = open('/etc/os-release', 'r').read().split('NAME="')[1].split('"')[0]
 
 if util.on_macos:
     VERSION_MAJOR_ID = '.'.join(platform.mac_ver()[0].split('.')[0:2])
     version = platform.mac_ver()[0].split('.')
     OS_VERSION = int(version[1]) + int(version[0]) * 1000
 elif util.on_windows or util.on_wsl:
-    POWERSHELL_PATH = '/mnt/c/Windows/System32/WindowsPowerShell/v1.0//powershell.exe'
     VERSION_MAJOR_ID = subprocess.run(['powershell.exe', '[Environment]::OSVersion.Version.Major'],
                                       capture_output=True, text=True).stdout.split('\n')[0]
     version = [VERSION_MAJOR_ID, 0]
+
 else:
     VERSION_MAJOR_ID = open(
         '/etc/os-release', 'r').read().split('VERSION_ID="')[1].split('"')[0]
@@ -41,14 +40,12 @@ else:
 
 if len(version) > 1:
     OS_VERSION = int(version[1]) + int(version[0]) * 1000
+
 else:
     OS_VERSION = int(version[0]) * 1000
 
 OS = f'{HOSTUNAME}_{NAME}'
-
 TOP_LEVEL_DOMAIN = (util.read_cache('tld') or 'docker').strip()
 DOCKER_CONTAINER_TAG = (util.read_cache('tag') or 'ns0').strip()
-DOCKER_CONTAINER_NAME = (util.read_cache(
-    'name') or DOCKER_CONTAINER_TAG).strip()
-
+DOCKER_CONTAINER_NAME = (util.read_cache('name') or DOCKER_CONTAINER_TAG).strip()
 SUPPORTED_OSES = json.load(open(f'{BASE_PATH}/supported_os.json', 'r'))
